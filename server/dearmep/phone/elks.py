11d7acde5da63fa7419a42e9c75b18b04b404704
iameru <post@i3o.eu>
2023-09-18 22:48:00 +0200

    Load phone numbers from 46elk on router startup
      - some error handling
      - relevant for #126

--- server/dearmep/phone/elks.py
+++ server/dearmep/phone/elks.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
-from typing import Literal, Tuple
+from typing import Tuple, List, Literal
+from pydantic import BaseModel
 
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 
@@ -29,12 +30,52 @@ FinalState = Literal["success", "failed", "busy"]
 PhoneNumber = str
 
 
+class Number(BaseModel):
+    category: Literal["fixed", "mobile", "voip"]
+    country: str
+    expires: datetime
+    number: str
+    capabilities: List[str]
+    cost: int
+    active: Literal["yes", "no"]
+    allocated: datetime
+    id: str
+
+
+# will be loaded in function `startup`
+phone_numbers: List[Number] = []
+
 # Helpers
+
 
 logger = logging.getLogger(__name__)
 
 
-async def verify_origin(request: Request):
+def get_numbers() -> List[Number]:
+    """
+    Fetches all available numbers to the account from 46elks.
+    """
+
+    response = requests.get(
+        url="https://api.46elks.com/a1/numbers",
+        auth=auth
+    )
+    if response.status_code != 200:
+        logger.error(
+            'Could not fetch numbers from 46elks. Response status: %s',
+            response.status_code
+        )
+        return []
+
+    numbers: List[Number] = [
+        Number.parse_obj(number) for number in response.json().get('data')
+    ]
+    logger.info(f"Fetched {len(numbers)} phone numbers from 46elks.")
+
+    return numbers
+
+
+def verify_origin(request: Request):
     client_ip = None if request.client is None else request.client.host
     if client_ip not in whitelisted_ips:
         logger.debug(f"refusing {client_ip}, not a 46elks IP")
@@ -47,13 +88,6 @@ async def verify_origin(request: Request):
         )
 
 
-# DEV only. keywords to find TODO print eval
-def get_function_name():
-    import inspect
-    frame = inspect.currentframe()
-    return frame.f_back.f_code.co_name
-
-
 # Routes
 
 router = APIRouter(
@@ -61,9 +95,20 @@ router = APIRouter(
 )
 
 
+@router.on_event("startup")
+async def startup():
+    for number in get_numbers():
+        phone_numbers.append(number)
+
+    if len(phone_numbers) == 0:
+        logger.error(
+            "No phone numbers were fetched on startup. "
+            "Please make sure you have configured your 46elks account."
+        )
+
+
 @router.get("/call")
 def explorative_calls():
-    _f = get_function_name()
 
     response = requests.get(
         url="https://api.46elks.com/a1/numbers",
@@ -73,7 +118,6 @@ def explorative_calls():
     response.raise_for_status()
 
     return {
-        "function": _f,
         "response.status_code": response.status_code,
         "response.text": response.text
     }
